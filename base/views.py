20ce2f8eacc992ca84271f8058219d116402acc3
ahmadDev <ahmadjafari6050@gmail.com>
2022-08-08 12:46:02 +0330

    add: templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    return HttpResponse('This is home page')
+rooms = [
+    {'id': 1, 'name': 'Learn python'},
+    {'id': 2, 'name': 'Learn django'},
+    {'id': 3, 'name': 'Learn frontend'},
+]
 
-def room(request):
-    return HttpResponse('This is room')
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html',  context)
